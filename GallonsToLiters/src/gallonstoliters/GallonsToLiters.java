65813a83c2b50a284dc1852c0bc4a4cda615e788
Garaix-Davy <dgaraix@gmail.com>
2015-01-12 15:08:08 -0800

    Added GallonsToLiters assignment

---
+++ GallonsToLiters/src/gallonstoliters/GallonsToLiters.java
@@ -0,0 +1,19 @@
+package gallonstoliters;
+
+/*
+    Try This 1-1
+ */
+
+public class GallonsToLiters {
+
+    public static void main(String args[]) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+
+}
